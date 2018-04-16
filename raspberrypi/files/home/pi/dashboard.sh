699a096e1e4d5bc4166877367121f31b1bb0ab68
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2018-04-16 16:34:45 +0900

    Add desktop launcher for dashboard.

--- raspberrypi/files/home/pi/dashboard.sh
+++ raspberrypi/files/home/pi/dashboard.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 xset -dpms
 xset s off
-#chromium-browser --app https://www.magellanic-clouds.com/
+
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-chromium-browser --noerrdialogs --kiosk https://www.magellanic-clouds.com/
+chromium-browser --noerrdialogs --window-position=0,0 --window-size=800,480 --kiosk https://www.google.com/
