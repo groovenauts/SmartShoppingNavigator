86a8d6a3f99b75147fabc8525625ba5533c445af
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2018-04-20 16:57:04 +0900

    Fix dashboard initial URL.

--- raspberrypi/files/home/pi/dashboard.sh
+++ raspberrypi/files/home/pi/dashboard.sh
@@ -6,4 +6,4 @@ xset s off
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-chromium-browser --noerrdialogs --window-position=0,0 --window-size=800,480 --kiosk https://www.google.com/
+chromium-browser --noerrdialogs --window-position=0,0 --window-size=800,480 --kiosk http://localhost:5000/
