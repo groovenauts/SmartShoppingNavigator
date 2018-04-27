5f90ba90725c486dc72a09e52470e02a732a9535
nakanori <n-nakamura@groovenauts.jp>
2018-04-27 17:51:40 +0900

    stash

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -1,11 +1,32 @@
 const INPUT_URL = "https://storage.googleapis.com/gcp-iost-images/annotated/picamera01/annotated.jpg";
 const INPUT_INTERVAL = 10000;
+const OUTPUT_INTERVAL = 10000;
 
 setTimeout(function() {
   $('#src_image').attr('src', INPUT_URL);
 }, INPUT_INTERVAL);
 
-}, SRC_IMG_INTERVAL);
+setTimeout(function() {
+  $.ajax({
+    url: '/displayByDevice',
+    data: {deivceId: "picamera01"},
+    dataType: 'html',
+  }).done(function(data, textStatus) {
+    console.log(data);
+    if (data) {
+      $('preview').contents().find('html').html(data);
+      $('.content-footer').text(moment().format('YYYY-MM-DD HH:mm:ss'));
+    //   let doc = document.getElementById('preview').contentWindow.document;
+    //   if (doc) {
+    //     doc.open();
+    //     doc.write(data);
+    //     doc.close();
+    //   }
+    }
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(new Date(), 'An error occurred.', textStatus);
+  });
+}, OUTPUT_INTERVAL);
 
 $(function() {
   $('select').change(function(e) {
@@ -14,14 +35,12 @@ $(function() {
       url: '/setting',
       type: 'POST',
       data: $('form').serialize(),
-      timeout: 1000 * 60,
-    }).done(function(data) {
+    }).done(function(data, textStatus) {
       console.log(new Date(), 'Submission was successful.');
       console.log(data);
-    }).fail(function(data) {
-      console.log(new Date(), 'An error occurred.');
-      console.log(data);
-    }).always(function(data) {
+    }).fail(function(xhr, textStatus, errorThrown) {
+      console.log(new Date(), 'An error occurred.', textStatus);
+    }).always(function() {
       enableForm();
     });
     e.preventDefault();
